06120c96bddcbec6786921b9b2002f4dac883652
Krish Khandelwal <kkhandelwal292@gmail.com>
2026-04-08 23:55:05 +0530

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -9,9 +9,13 @@ def expected_value_discrete(x, p):
     P = np.array(p)
     if len(x) != len(p):
         raise ValueError("x and p must have the same length")
-    if not np.isclose(np.sum(p),1):
+        
+    if not np.isclose(np.sum(p),1): 
         raise ValueError("Value Error,Sum of probabilities should be 1")
-    y=0.0
+        
+## np.isclose is used to check whether two numbers are approximately equal, not exactly equal.as in python 0.1 + 0.2 != 0.3
+    
+    y=0
     for i in range(len(x)):
         y = y+x[i]*p[i]
 
